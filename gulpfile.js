a2c3160e4bfef130cc11997bed6ba762770f4201
laiker <root@localhost.localdomain>
2018-11-11 23:54:10 +0700

    less support added

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+
+gulp.task("less", function(){
+    gulp.src("less/style.less")
+        .pipe(less())
+        .pipe(gulp.dest("css"));
+})
+
+gulp.task('default', [ 'less' ]);
+
